ced7cfbb00d3c627e2a314ff4a47c1189538bfe1
camorton <69657334+camorton@users.noreply.github.com>
2020-12-14 17:31:34 -0500

    added matching js

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
+//Match Card
 
+const cards = document.querySelectorAll('.cards-container') //or do I need to add another class for all cards?  What are we using  cards-container for?
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
+function flipCard() {
+    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if(hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unFlipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+//shuffling
+
+function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 16);
+        card.getElementsByClassName.order = randomPos;
+    });
+}
 
 
 
